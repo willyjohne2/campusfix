51e03efaa5e276eae6edfed9b98a9c2068752845
willyjohne2 <johnfaii285@gmail.com>
2026-01-27 11:45:17 +0300

    Add WhiteNoise, STATIC_ROOT, update Procfile, add requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn campusFix.wsgi --log-file -
+web: gunicorn campusFix.wsgi:application --bind 0.0.0.0:$PORT --workers 3
--- campusFix/settings.py
+++ campusFix/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -179,6 +180,10 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+# Location where `collectstatic` will collect static files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# Use WhiteNoise storage for compressed static files in production
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media files (User uploads)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
